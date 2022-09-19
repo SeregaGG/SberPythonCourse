b458eae62458fdf5ba00a8bcd87eb44c5465e7cc
SeregaGG <48214766+SeregaGG@users.noreply.github.com>
2022-09-20 02:37:25 +0500

    3 part

--- Pytorial/main.py
+++ Pytorial/main.py
@@ -1,18 +1,19 @@
 import time
 
-import numpy
 from matplotlib import pyplot
 
+from visualizer.visualizer import Visualizer
 from our_home_parser import parser
 from cleaner.cleaner import Cleaner
 
 if __name__ == '__main__':
     start_time = time.time()
 
-    parser = parser.Parser(total_count=4000, offset_=1, limit_=100)
+    parser = parser.Parser(total_count=10000, offset_=1, limit_=100)
 
-    header = ['region', 'objPriceAvg', 'floorMax', 'objReady100PercDt', 'objElemLivingCnt', 'objFlatSq', 'objElemParkingCnt']
-            #  регион,  цена за м2,   кол-во этажей, ввод в эксплуатацию?, кол-во квартир, жилая площадь, кол-во парк. мест
+    header = ['region', 'objPriceAvg', 'floorMax', 'objReady100PercDt', 'objElemLivingCnt', 'objFlatSq',
+              'objElemParkingCnt']
+    #  регион,  цена за м2,   кол-во этажей, ввод в эксплуатацию?, кол-во квартир, жилая площадь, кол-во парк. мест
     df = parser.save_to_data_frame(header)
 
     not_zero_params = [('objElemLivingCnt', int), ('objFlatSq', float)]
@@ -22,10 +23,7 @@ if __name__ == '__main__':
     print(clean_df.dtypes)
     clean_df.to_csv('test.csv', sep='\t', encoding='cp1251', columns=header)
 
-    test_df = clean_df.groupby(['region']).aggregate(numpy.average)
-    test_df2 = clean_df.groupby(['region']).aggregate(numpy.count_nonzero)
-    test_df2.plot(y='objPriceAvg', kind='bar')
-    print(test_df.head(100))
-    test_df.plot(y='objPriceAvg', kind='bar')
-    pyplot.show()
     print(time.time() - start_time)
+    Visualizer.make_plot(clean_df)
+
+    pyplot.show()
--- Pytorial/visualizer/visualizer.py
+++ Pytorial/visualizer/visualizer.py
@@ -1,3 +1,23 @@
+import datetime
+
+import numpy
+from pandas import DataFrame
+
+
 class Visualizer:
-    def __init__(self):
-        pass
+    @staticmethod
+    def make_plot(clean_df: DataFrame):
+        avg_df_values = clean_df.groupby(['region']).aggregate(numpy.average)
+        avg_df_values.plot(y='objPriceAvg', kind='bar').set_ylabel('Avg price')
+        avg_df_values.plot(y='floorMax', kind='bar').set_ylabel('Avg max floor')
+        avg_df_values.plot(y='objElemLivingCnt', kind='bar').set_ylabel('Avg apartment count')
+        avg_df_values.plot(y='objFlatSq', kind='bar').set_ylabel('Avg flat square')
+        avg_df_values.plot(y='objElemParkingCnt', kind='bar').set_ylabel('Avg parking count')
+
+        df_count_of_elements = clean_df.groupby(['region']).aggregate(numpy.count_nonzero)
+        df_count_of_elements.plot(y='objPriceAvg', kind='bar').set_ylabel('Build count')
+
+        clean_df['objReady100PercDt'] = [datetime.datetime.strptime(x, "%Y-%m-%d").year for x in
+                                         clean_df['objReady100PercDt']]
+        test_df3 = clean_df.groupby(['objReady100PercDt']).aggregate(numpy.count_nonzero)
+        test_df3.plot(y='objPriceAvg', kind='bar').set_ylabel('Build count')
