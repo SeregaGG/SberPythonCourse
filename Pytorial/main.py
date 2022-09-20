ecf7203c025871218d516661507819bc53cf2499
SeregaGG <48214766+SeregaGG@users.noreply.github.com>
2022-09-21 03:15:11 +0500

    change save to file

--- Pytorial/main.py
+++ Pytorial/main.py
@@ -1,5 +1,6 @@
 import time
 
+import pandas
 from matplotlib import pyplot
 
 from visualizer.visualizer import Visualizer
@@ -9,7 +10,7 @@ from cleaner.cleaner import Cleaner
 if __name__ == '__main__':
     start_time = time.time()
 
-    parser = parser.Parser(total_count=10000, offset_=1, limit_=100)
+    parser = parser.Parser(total_count=100, offset_=1, limit_=100)
 
     header = ['region', 'objPriceAvg', 'floorMax', 'objReady100PercDt', 'objElemLivingCnt', 'objFlatSq',
               'objElemParkingCnt']
@@ -21,7 +22,10 @@ if __name__ == '__main__':
     clean_df = Cleaner.clean_zero_val(clean_df, not_zero_params)
 
     print(clean_df.dtypes)
-    clean_df.to_csv('test.csv', sep='\t', encoding='cp1251', columns=header)
+    writer = pandas.ExcelWriter('test.xlsx')
+    clean_df.to_excel(writer)
+    writer.save()
+    #clean_df.to_excel('test.csv', sep='\t', encoding='cp1251', columns=header)
 
     print(time.time() - start_time)
     Visualizer.make_plot(clean_df)
